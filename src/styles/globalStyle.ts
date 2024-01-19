6d421b9d4fab2bd96ba5aa7b840d1e55e2c3b7c0
suicxyz <suicxyz@proton.me>
2024-01-19 08:17:21 -0300

    add global styles configuration file

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    height: 100%;
+  }
+`;
+ 
+export default GlobalStyle;
